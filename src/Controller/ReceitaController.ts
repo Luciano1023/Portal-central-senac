06af25e9ef27dc9f8e630541dadf68e9fb016d79
Bielzinn-9 <bielxbox0112@gmail.com>
2026-08-05 11:16:08 -0300

    correção na import repository

--- src/Controller/ReceitaController.ts
+++ src/Controller/ReceitaController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { NoticiaRepository } from "../Repositories/ReceitaRepository";
+import { NoticiaRepository } from "../Repositories/NoticiaRepository";
 import { Noticia } from "../Models/Noticia";
 
 const repository = new NoticiaRepository();
