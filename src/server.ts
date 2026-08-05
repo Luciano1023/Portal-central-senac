62af676a98f24527436866d9c008baf548f02a64
vitorfilgueiraao-cmd <vitorfilgueiraao@gmail.com>
2026-08-05 10:58:20 -0300

    server ts atualizado

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
 import app from "./app";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-<<<<<<< HEAD
     console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-=======
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
->>>>>>> 75d9ff58acfd1397f87af39ff6cf890c529626e5
 });
