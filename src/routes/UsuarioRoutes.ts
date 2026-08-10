d253b41c97b96d67d179ad0aec9ffd2e1d8f66a4
Vitor Filgueira <vitorfilgueiraao@gmail.com>
2026-08-10 18:27:31 -0300

    Add authentication middleware to usuarioRoutes

--- src/routes/UsuarioRoutes.ts
+++ src/routes/UsuarioRoutes.ts
@@ -1,13 +1,28 @@
 import { Router } from "express";
 import { UsuarioController } from "../Controller/UsuarioController";
+import { autenticar } from "../middlewares/authMiddlewares";
 
 const usuarioRoutes = Router();
 const controller = new UsuarioController();
 
-usuarioRoutes.get("/", (req, res) => controller.listar(req, res));
-usuarioRoutes.get("/:id", (req, res) => controller.buscar(req, res));
-usuarioRoutes.post("/", (req, res) => controller.cadastrar(req, res));
-usuarioRoutes.put("/:id", (req, res) => controller.editar(req, res));
-usuarioRoutes.delete("/:id", (req, res) => controller.remover(req, res));
+usuarioRoutes.get("/", autenticar, (req, res) =>
+    controller.listar(req, res)
+);
+
+usuarioRoutes.get("/:id", autenticar, (req, res) =>
+    controller.buscar(req, res)
+);
+
+usuarioRoutes.post("/", autenticar, (req, res) =>
+    controller.cadastrar(req, res)
+);
+
+usuarioRoutes.put("/:id", autenticar, (req, res) =>
+    controller.editar(req, res)
+);
+
+usuarioRoutes.delete("/:id", autenticar, (req, res) =>
+    controller.remover(req, res)
+);
 
 export { usuarioRoutes };
