059646ab9e1295ac34a0d3edd513c0d295e01648
Vitor Filgueira <vitorfilgueiraao@gmail.com>
2026-08-10 18:31:55 -0300

    Add authentication routes for login, register, and logout

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -5,22 +5,27 @@ export const authRoutes = Router();
 
 const controller = new AuthController();
 
+// Página de login
 authRoutes.get("/login", (req, res) => {
     res.render("login");
 });
 
+// Realizar login
 authRoutes.post("/login", (req, res) => {
     controller.login(req, res);
 });
 
+// Página de cadastro
 authRoutes.get("/cadastro", (req, res) => {
     res.render("cadastro");
 });
 
+// Realizar cadastro
 authRoutes.post("/cadastro", (req, res) => {
     controller.cadastrar(req, res);
 });
 
+// Realizar logout
 authRoutes.post("/logout", (req, res) => {
     controller.logout(req, res);
 });
