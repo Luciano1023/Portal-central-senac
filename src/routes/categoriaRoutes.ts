ba05499b21dee6fbd22bdb05245d5e74bec604cb
Vitor Filgueira <vitorfilgueiraao@gmail.com>
2026-08-10 18:29:02 -0300

    Add authentication middleware to categoriaRoutes

--- src/routes/categoriaRoutes.ts
+++ src/routes/categoriaRoutes.ts
@@ -1,13 +1,28 @@
 import { Router } from "express";
 import { CategoriaController } from "../Controller/CategoriaController";
+import { autenticar } from "../middlewares/authMiddlewares";
 
 const categoriaRoutes = Router();
 const controller = new CategoriaController();
 
-categoriaRoutes.get("/", (req, res) => controller.listar(req, res));
-categoriaRoutes.get("/:id", (req, res) => controller.buscar(req, res));
-categoriaRoutes.post("/", (req, res) => controller.cadastrar(req, res));
-categoriaRoutes.put("/:id", (req, res) => controller.editar(req, res));
-categoriaRoutes.delete("/:id", (req, res) => controller.remover(req, res));
+categoriaRoutes.get("/", autenticar, (req, res) =>
+    controller.listar(req, res)
+);
+
+categoriaRoutes.get("/:id", autenticar, (req, res) =>
+    controller.buscar(req, res)
+);
+
+categoriaRoutes.post("/", autenticar, (req, res) =>
+    controller.cadastrar(req, res)
+);
+
+categoriaRoutes.put("/:id", autenticar, (req, res) =>
+    controller.editar(req, res)
+);
+
+categoriaRoutes.delete("/:id", autenticar, (req, res) =>
+    controller.remover(req, res)
+);
 
 export default categoriaRoutes;
